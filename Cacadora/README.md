5c63967cff7555710fd79ec8684d999366d309b3
eltonsrgit <167440350+eltonsrgit@users.noreply.github.com>
2025-08-04 20:38:04 -0300

    Add files via upload

---
+++ Cacadora/README.md
@@ -0,0 +1,2 @@
+# Ultima atualização:
+Segunda, 19 de Agosto de 2024. Depois da competição de robótica da Rio Innovation Week.
---
+++ Cacadora/sensoresIR.h
@@ -0,0 +1,29 @@
+#ifndef sensoresIR_H
+#define sensoresIR_H
+
+#define leftIRpin 32
+#define rightIRpin 26
+
+
+bool LeftDetection(){
+  bool left_IR_read = digitalRead(leftIRpin);
+  if (left_IR_read == 0){
+    return false; // sem inimigo
+  }
+  else{
+    return true; // com inimigo
+  }
+  
+}
+
+bool RightDetection(){
+  bool right_IR_read = digitalRead(rightIRpin);
+  if (right_IR_read == 0){
+    return false; // sem inimigo0
+  }
+  else{
+    return true;  // com inimigo
+  }
+}
+
+#endif
